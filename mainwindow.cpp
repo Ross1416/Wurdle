4931b340b85c0d12cd94ad626752cb9c352780d3
Ross <77679842+Ross1416@users.noreply.github.com>
2022-03-09 18:25:42 +0000

    Testing git 2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -94,7 +94,7 @@ void MainWindow::SetupValidWordsScrollArea()
 
 void MainWindow::SetupUsefulWordsScrollArea()
 {
-// testing change
+// testing change 2
 }
 
 void MainWindow::SetupLetterContainer(int w, int h)
