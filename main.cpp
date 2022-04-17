3f01166060b9b8f0b810f8af6eecf40ef64acabd
Ross Inglis <r.inglis617@gmail.com>
2022-04-17 14:24:24 +0100

    moved some lines about in main.cpp. changed some comments

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
-#include "mainwindow.h"
 #include <QApplication>
 
+#include "mainwindow.h"
 #include "game.h"
 #include "settingsmenu.h"
-#include <iostream>
-//#include<QTranslator>
-
 
 int main(int argc, char *argv[])
 {
-    Game currentGame(5, 6); //A game object is initialised with the default number of characters 5 and the default max guesses 6
-
-    // Initialise QT application
-    QApplication a(argc, argv);
+    Game currentGame(5, 6); // A game object is initialised with the default number
+                            // of characters 5 and the default max guesses 6
 
     // Initialise settings file
     SettingsFileHandler settingsFile = SettingsFileHandler("settings.txt");
     settingsFile.read();
+
+    // Initialise QT application
+    QApplication a(argc, argv);
 
     // Initialise MainWindow
     MainWindow w(&currentGame, &settingsFile);
     w.show();
     return a.exec();
 }
+
+
+
